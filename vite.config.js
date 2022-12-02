5140d69eb207e9799df1d211abc2b8a28e484ad7
Wa_Fe <www.520x.com@gmail.com>
2022-12-02 14:13:50 +0800

    fix:增加自动引入的配置

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,49 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Wa_Fe
  * @Date: 2022-10-13 15:28:43
  * @LastEditTime: 2022-10-13 17:31:35
  * @LastEditors: Wa_Fe
  */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 // https://vitejs.dev/config/
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
 export default defineConfig({
-  plugins: [vue(),'vue/compiler-sfc'],
+  plugins: [
+    vue(),
+    "vue/compiler-sfc",
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ["vue", "vue-router"],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dirs: ["src/components"],
+    }),
+  ],
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './'),
-      '@': path.resolve(__dirname, 'src')
+      "~": path.resolve(__dirname, "./"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   server: {
-    port: '3000',
-    open: false, //自动打开 
+    port: "3000",
+    open: false, //自动打开
     base: "./ ", //生产环境路径
-    proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
+    proxy: {
+      // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
       // 正则表达式写法
-      '^/api': {
+      "^/api": {
         target: "http://tool.api.bancoulm.com/", // 后端服务实际地址
         changeOrigin: true, //开启代理
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
