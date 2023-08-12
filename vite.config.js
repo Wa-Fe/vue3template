39308c6b481609e87d43ab53420a835514e69bac
Wa_Fe <www.520x.com@gmail.com>
2023-08-12 14:09:23 +0800

    feat: 完善打包的配置

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 // 打包分析
 import { visualizer } from "rollup-plugin-visualizer"
 
+// 打包压缩
+import viteCompression from "vite-plugin-compression"
+
 // eslint
 import eslintPlugin from "vite-plugin-eslint"
 
@@ -28,6 +31,7 @@ export default defineConfig({
       // 按需引入的文件的类型
       "extensions": ["vue"],
     }),
+    // 分析
     visualizer({
       "gzipSize": true,
       "brotliSize": true,
@@ -35,9 +39,19 @@ export default defineConfig({
       "filename": "analyze.html", //分析图生成的文件名
       "open": false //如果存在本地服务端口，将在打包后自动展示
     }),
+    // eslint
     eslintPlugin({
       "include": ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
     }),
+    // 打包配置
+    viteCompression({
+      "verbose": true, //是否在控制台输出压缩结果
+      "disable": false, //是否禁用压缩
+      "deleteOriginFile":true, //压缩后是否删除原文件
+      "threshold": 10240, //启用压缩的文件大小限制，单位是字节
+      "algorithm": "gzip", //采用的压缩算法
+      "ext": ".gz", //生成的压缩包后缀
+    })
   ],
   "resolve": {
     "alias": {
