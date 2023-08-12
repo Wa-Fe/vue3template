c84a090588e1d5cb41856dd525c736983c98b108
Wa_Fe <www.520x.com@gmail.com>
2023-08-12 16:19:24 +0800

    2023-08-12 16:19:24

--- publishD.sh
+++ publishD.sh
@@ -11,6 +11,7 @@ npm run build:dev
 
 # 切换到dis目录
 cd dist/
+echo "进入disk目录"
 
 
 # git部分
@@ -19,12 +20,15 @@ git init
 git add -A
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m $time
-
+echo "正在上传"
 # 发布到分支
 git push -f git@github.com:Wa-Fe/vue3vue3template.git main:public
 
 
+
 cd ..
+
+echo "发布完成"
 
 
 
